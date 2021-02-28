aa5f45f56f5befcf92518acdf731b6e972da1653
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-27 23:43:33 -0500

    alpha semantics

--- sm.js
+++ sm.js
@@ -77,6 +77,98 @@ function getNamedFile (fname) {
     }	
 }
 
+////////////
+function addTranspiler (sem) {
+    sem.addOperation (
+	"js",
+	{
+	    Main : function (_1) { return _1.js (); }, // StateMachine
+	    
+	    StateMachine : function (_1, _2, _3, _4) { // NameSection InputSection OutputSection MachineSection
+		var nameSection = _1.js ();
+		var inputSection = _2.js ();
+		var outputSection = _3.js ();
+		var machineSection = _4.js ();
+		return machineSection;
+	    },
+	    
+	    NameSection : function (_1, _2, _3) {}, // "name" ":" Name
+	    InputSection : function (_1, _2, _3) {}, // "inputs" ":" InputPinNames
+	    OutputSection : function (_1, _2, _3) {}, // "outputs" ":" OutputPinNames
+	    
+	    MachineSection : function (_1, _2s, _3) { // Header State+ Trailer
+		var machineName = _1.js ();
+		var triples = _2s.js ();
+		var stateCode = triples.map (triple => { return triple.step; }).join ('\n');
+		var defaultState = triples.map (triple => { return triple.defaultState; }).join ('\n');
+		var entryCode = triples.map (triple => { return triple.entry; }).join ('\n');
+		
+		var smCode = `
+function ${machineName} () {
+  this.state = null;
+  this.step = function (event) {
+    switch (this.state) {
+      ${stateCode}
+      default: ${defaultState}
+    };
+  this.enter = function (next_state) {
+    switch (next_state) {
+      ${entryCode}
+    }
+  }
+ }
+}
+`;
+		return smCode;
+	    },
+	    
+	    Header : function (_1, _2, _3) { return _2.js (); }, // "machine" MachineName ":"
+	    Trailer : function (_1, _2) {return "";}, // "end" "machine"
+	    
+	    State : function (_1, _2, _3, _4, _5s) {  // "state" Name ":" EntrySection Transition*
+		var name = _2.js ();
+		var entry = _4.js ();
+		var transitions = _5.js ();
+		var stepcode = `case ${name}:\n${transitions}\nbreak;`;
+		var entrycode = `case ${name}:\n${entry}\nbreak;`;
+		return { step: stepcode, entry: entrycode, defaultState: "" };
+	    },
+	    EntrySection : function (_1, _2, _3) {return _3.js ()}, // "entry" ":" string
+	    Transition : function (_1, _2, _3, _4, _5) {}, // "on" Name ":" "next" Name
+	    
+	    
+	    
+	    keyword : function (_1) {return _1.js ()}, // "machine" | "name" | "inputs" | "outputs" | "end" | "state" | "entry" | "on" | "next" | "default"
+	    InputPinNames : function (_1) {return _1.js ()}, // nameList
+	    OutputPinNames : function (_1) {return _1.js ()}, // nameList
+	    MachineName : function (_1) {return _1.js ()}, // Name
+	    StateName : function (_1) {return _1.js ()}, // Name
+	    InputPinReference : function (_1) {return _1.js ()}, // Name
+	    StateReference : function (_1) {return _1.js ()}, // Name
+	    Name : function (_1) {return _1.js ()}, // ~keyword id
+	    nameList : function (_1s, _2s) { // (~keyword id delim)+
+		return _1s.js ().map (name => {
+		    inputCounter += 1;
+		    return `const ${name} = ${inputCounter};`;
+		});
+	    }, 
+	    
+	    
+	    
+	    id : function (_1, _2s) { return `${_1.js ()}${_2s.js ().join ('')}` }, // firstId followId*
+	    firstId : function (_1) {return _1.js ()}, // "A".."Z" | "a".."z" | "_"
+	    followId : function (_1) {return _1.js ()}, // firstId
+	    
+	    string : function (_1, _2s, _3) { return `${_2s.js ().join ('');}`; }, // "\\"" stringChar* "\\""
+	    escapedChar : function (_1, _2) { return _2.js (); }, // "\\\\" any
+	    anyStringChar : function (_1) {return _1.js ();}, // ~"\\"" any
+	    
+	    delim : function (_1s) {return _1s.js ().join (''); }, // (" " | "\\t" | "\\n")+
+	    
+	    _terminal: function () { return this.primitiveValue; }
+	});
+}
+////////////
 
 var result = main ();
 console.log (result.toString ());

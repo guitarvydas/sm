87e859d170faf472072878168ad423d7eec88343
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-12 15:02:11 -0500

    wip making list of transitions

--- x.pl
+++ x.pl
@@ -1,8 +1,12 @@
-transition( id="yK0KRwzggwMbkE22hVbx-13", parent="yK0KRwzggwMbkE22hVbx-1", source="yK0KRwzggwMbkE22hVbx-17", target="yK0KRwzggwMbkE22hVbx-20").
+:- discontiguous transitionCode/3.
+:- discontiguous transition/4.
+
+transition(id="yK0KRwzggwMbkE22hVbx-13",parent="yK0KRwzggwMbkE22hVbx-1",source="yK0KRwzggwMbkE22hVbx-17",target="yK0KRwzggwMbkE22hVbx-20").
 transitionCode( id="yK0KRwzggwMbkE22hVbx-14", parent="yK0KRwzggwMbkE22hVbx-13", value="x > w<div>{reverse ()}</div>").
+transition(id="yK0KRwzggwMbkE22hVbx-15",parent="yK0KRwzggwMbkE22hVbx-1",source="yK0KRwzggwMbkE22hVbx-17",target="yK0KRwzggwMbkE22hVbx-23").
+transitionCode(id="yK0KRwzggwMbkE22hVbx-16",parent="yK0KRwzggwMbkE22hVbx-15",value="x<0 {reverse()}").
 state( id="yK0KRwzggwMbkE22hVbx-17", parent="yK0KRwzggwMbkE22hVbx-1", value="idle").
 
-lab(P,C,D,Name):-
-    transitionCode(id=_,parent=P,value=C),
-    transition(id=P,parent=_,source=S,target=D),
-    state(id=S,parent=_,value=Name).
+assocCode(Tr,C,S,D):-
+    transitionCode(id=_,parent=Tr,value=C),
+    transition(id=Tr,parent=_,source=S,target=D).

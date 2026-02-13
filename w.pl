66cfdaf01f0334e12c2604ef3c2343cb134aedfc
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-13 11:35:42 -0500

    10.sm emitted OK

--- w.pl
+++ w.pl
@@ -1,21 +1,23 @@
-emitState(Diagram,IDstate):-
-    state(did=Diagram, id=IDstate, parent=_, value=NamePlusCode),
-    format("state ~q{~n", [NamePlusCode]),
+emitTransitions(Diagram,IDstate,Dest,Guard,Tcode):-
     forall(
-        transition(did=Diagram, id=_, parent=_, source=IDstate, target=Dest, code=C),
-	state(did=Diagram ... need to parse off enter/step/exit/name from C ...
-        format("%nextif {~q}~n",[C,Dest])
-    ),
-    format("}~n",[]).
+        transition(did=Diagram, id=_, parent=_, source=IDstate, target=Dest, guard=Guard, transitioncode=Tcode),
+        format("%next ~q %if ~w ~w~n",[Dest,Guard,Tcode])
+    ).
+
+emitState(Diagram,IDstate):-
+    state(did=Diagram, id=IDstate, parent=_, name=Name,enter=Enter,exit=Exit),
+    format("state ~q {~n{~w}~n{~n", [Name, Enter]),
+    emitTransitions(Diagram,IDstate,_,_,_),
+    format("}~n{~w}~n}~n",[Exit]).
 
 emitAllStates(Diagram):-
     forall(
-	state(did=Diagram, id=IDstate, parent=_, value=_),
+	state(did=Diagram, id=IDstate, parent=_, name=_, enter=_, exit=_),
 	emitState(Diagram,IDstate)
     ).
 
 emit:-
-    consult("8.pl"),
+    consult("9.pl"),
     forall(
 	diagram(did=Diagram),
 	emitAllStates(Diagram)

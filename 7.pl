d0c971e5b65131ad1c9254ac3eb732b671c5debb
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-13 08:57:49 -0500

    save wip 9.pl

--- 7.pl
+++ 7.pl
@@ -3,9 +3,9 @@
 :- discontiguous state/4.
 diagram( did=i4kYwzf).
 transition( did=i4kYwzf, id=i13, parent=i1, source=i17, target=i20).
-trCode( did=i4kYwzf, id=i14, parent=i13, value="x > w{reverse ()};").
+trCode( did=i4kYwzf, id=i14, parent=i13, value="x > w{reverse ()}").
 transition( did=i4kYwzf, id=i15, parent=i1, source=i17, target=i23).
-trCode( did=i4kYwzf, id=i16, parent=i15, value="x < 0{reverse ()};").
+trCode( did=i4kYwzf, id=i16, parent=i15, value="x < 0{reverse ()}").
 state( did=i4kYwzf, id=i17, parent=i1, value="idle").
 transition( did=i4kYwzf, id=i18, parent=i1, source=i20, target=i17).
 trCode( did=i4kYwzf, id=i19, parent=i18, value="x <= w").

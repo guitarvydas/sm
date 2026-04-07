1bc942b37a3144465bc8e390a05bc685315df991
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-04-07 05:43:46 -0400

    explicitly supply context to sm class

--- looptest.py
+++ looptest.py
@@ -1,38 +1,48 @@
 class SM_looptest:
     
     def enter_idle (self):
+        c = self.ctx
         self.state = "idle"
         
     def step_idle (self):
-        if (x > w):
+        c = self.ctx
+        if (c.x > c.w):
             self.exit_idle ()
-            reverse ()
+            c.reverse ()
             self.enter_wait_for_w_recrossing ()
-        if (x < 0):
+        if (c.x < 0):
             self.exit_idle ()
-            reverse ()
+            c.reverse ()
             self.enter_wait_for_zero_recrossing ()
     def exit_idle (self):
+        c = self.ctx
         pass
     def enter_wait_for_w_recrossing (self):
+        c = self.ctx
         self.state = "wait for w recrossing"
         
     def step_wait_for_w_recrossing (self):
-        if (x <= w):
+        c = self.ctx
+        if (c.x <= c.w):
             self.exit_wait_for_w_recrossing ()
             self.enter_idle ()
     def exit_wait_for_w_recrossing (self):
+        c = self.ctx
         pass
     def enter_wait_for_zero_recrossing (self):
+        c = self.ctx
         self.state = "wait for zero recrossing"
         
     def step_wait_for_zero_recrossing (self):
-        if (x >= 0):
+        c = self.ctx
+        if (c.x >= 0):
             self.exit_wait_for_zero_recrossing ()
             self.enter_idle ()
     def exit_wait_for_zero_recrossing (self):
+        c = self.ctx
         pass
-    def __init__ (self):
+    def __init__ (self, context):
+        self.ctx = context
         self.state = None
         self.enter_idle ()
     def step (self):
@@ -41,5 +51,3 @@ class SM_looptest:
             "wait for w recrossing": self.step_wait_for_w_recrossing,
             "wait for zero recrossing": self.step_wait_for_zero_recrossing,
         } [self.state] ()
-        
-sm = SM_looptest ()
--- tester.py
+++ tester.py
@@ -1,9 +1,11 @@
-import looptest
-
-uut = looptest.SM_looptest () # uut = unit under test
+import sys
 
 x = 0
 w = 100
+
+import looptest
+
+uut = looptest.SM_looptest(context=sys.modules['__main__']) # uut == unit under test
 
 def reverse ():
     print (f'reverse')
